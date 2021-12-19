6b5e48c2105ff0d01d4ae7a64e18649554f3d79b
ezegavilan <ezegavilan.developer@gmail.com>
2021-12-18 21:37:59 -0300

    update to JUnit5 (fucking old mvn template)

--- src/test/java/com/gavilan/currency/MoneyTest.java
+++ src/test/java/com/gavilan/currency/MoneyTest.java
@@ -1,10 +1,9 @@
 package com.gavilan.currency;
 
 import com.gavilan.currency.domain.Dollar;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
 
